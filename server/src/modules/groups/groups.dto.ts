87f23043182cd4c4d2da5f1a90e0a96c76942ba3
Vladislav-Evg-Sid <vlad.s200@mail.ru>
2026-08-17 22:10:10 +0500

    rm type from shared/ to types/

--- server/src/modules/groups/groups.dto.ts
+++ server/src/modules/groups/groups.dto.ts
@@ -1,6 +1,6 @@
 import { type Request } from "express";
 
-import { type NoParams } from "../../shared/types.js";
+import { type NoParams } from "../../types/requests.js";
 import { type GroupData } from "./groups.types.js";
 
 export type GetUserGroupsRequstDTO = Request<NoParams, GroupData[]>;
